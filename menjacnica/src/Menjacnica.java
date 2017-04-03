20e1c6a4769cd856bc2b603e711946a5ba38e9ed
stefan <vojvodicstefan96@gmail.com>
2017-04-03 19:11:06 +0200

    implementirana metoda dodaj kurs u klasi Menjacnica

--- menjacnica/src/Menjacnica.java
+++ menjacnica/src/Menjacnica.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 import specinterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private List<Valuta> valute = new ArrayList<>();
+	
 	@Override
 	public void dodajKurs(String skraceniNaziv, String duziNaziv, GregorianCalendar datum, Kurs kurs) {
-		// TODO Auto-generated method stub
-
+		Valuta v = new Valuta();
+		try {
+			v.setDatum(datum);
+			v.setKurs(kurs);
+			v.setNaziv(duziNaziv);
+			v.setSkraceniNaziv(skraceniNaziv);
+		} catch (RuntimeException e) {
+			throw new RuntimeException("Neka od vrednosti nije validna");
+		}
+		
 	}
 
 	@Override
