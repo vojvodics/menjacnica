1bf11fb3f69402bf89c8e2f8b990422b913f78d3
stefan <vojvodicstefan96@gmail.com>
2017-04-03 19:05:38 +0200

    Implementirane metode iz interfejsa MenjacnicaInterfejs

--- menjacnica/src/Menjacnica.java
+++ menjacnica/src/Menjacnica.java
@@ -1,26 +1,48 @@
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 import specinterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private List<Valuta> valute = new ArrayList<>();
+
 	@Override
 	public void dodajKurs(String skraceniNaziv, String duziNaziv, GregorianCalendar datum, Kurs kurs) {
-		// TODO Auto-generated method stub
-
+		Valuta v = new Valuta();
+		v.setDatum(datum);
+		v.setKurs(kurs);
+		v.setNaziv(duziNaziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		this.valute.add(v);
 	}
 
 	@Override
 	public Kurs izbrisiKursValute(String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+		Kurs k = new Kurs();
+		for (Valuta v : this.valute) {
+			if (v.getDatum().equals(datum) && v.getSkraceniNaziv().equals(skraceniNaziv)) {
+				k = v.getKurs();
+				this.valute.remove(v);
+				return k;
+			}
+		}
+
+		throw new RuntimeException("Trazena valuta ne postoji");
 	}
 
 	@Override
 	public Kurs nadjiKurs(String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Valuta v : this.valute) {
+			if (v.getDatum().equals(datum) && v.getSkraceniNaziv().equals(skraceniNaziv)) {
+				return v.getKurs();
+			}
+		}
+
+		throw new RuntimeException("Trazena valuta ne postoji");
 	}
 
 }
